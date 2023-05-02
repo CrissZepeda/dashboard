6e79cbada0c7bf4ac3102900adea74de7c1ccf9d
CrissZepeda <cristhian@difusa.cl>
2023-05-02 17:37:06 -0400

    Correción sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -12,12 +12,13 @@ function Sidebar() {
 
     return (
         <>
-            <div  className={`fixed lg:static w-[80%] md:w-[40%] lg:w-full bg-white transition-all top-0 ${sidebar ? "-left-0" : "-left-full"} -left-full w-full h-full overflow-y-auto lg:overflow-hidden col-span-1 py-9 flex flex-col gap-4 p-8 shadow-2xl`}>
-                
+            <div  className={`fixed lg:static w-[80%] md:w-[60%] lg:w-full bg-white top-0 ${sidebar ? "-left-0" : "-left-full"} w-full h-full overflow-y-auto lg:overflow-hidden col-span-1 py-9 flex flex-col gap-4 p-8 shadow-2xl`}>
+                {/* <button className="block absolute lg:hidden top-3 right-4 bg-purple-600 text-white text-3xl rounded-full p-3 shadow-lg" onClick={collapse}>
+                    <RiMenuFoldFill />
+                </button> */}
                 <div className="img_logo flex justify-center tracking-[4px] ">
                     LOGO
                 </div>
-
                 <nav>
                     <ul className="flex flex-col gap-5 text-lg">
                         <li className="font-semibold py-4 px-10 text-gray-400 hover:bg-purple-700 rounded-lg hover:text-white transition-colors">
@@ -47,9 +48,7 @@ function Sidebar() {
                         </li>
                     </ul>
                 </nav>
-
                 <img className="my-20" src={imgSideBar} alt="" />
-
             </div>
             <button className="block absolute lg:hidden top-3 right-4 bg-purple-600 text-white text-3xl rounded-full p-3 shadow-lg" onClick={collapse}>
                 {sidebar ? <RiCloseLine /> : <RiMenu5Fill />}                
